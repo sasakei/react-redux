7b626df887adb454398e347b0c57fa29dae894d9
sasaki kei <sasakei1021@icloud.com>
2020-06-11 11:00:49 +0900

    redux-basics

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMRNT = 'INCREMENT'
+export const DECREMRNT = 'DECREMENT'
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
